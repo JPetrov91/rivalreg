81539941751c266873ddf242966a6ec501390260
JPetrov91 <j.petrov91@gmail.com>
2017-01-15 01:03:33 +0200

    -DB Connect with PDO + first Classes

--- database/database.php
+++ database/database.php
@@ -1,11 +1,41 @@
 <?php
-class Database {
-    private $connection;
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2017.01.14.
+ * Time: 22:12
+ */
+
+namespace Database;
 
 
-    public function __construct($server, $login, $password, $database)
+class database
+{
+
+    protected $dbh;
+
+    public function __construct()
     {
-        $this->connection = mysqli_connect($server, $login, $password, $database);
+        try {
+            $this->dbh = new \PDO('mysql:host=localhost;dbname=rival;port=3308', 'root', '');
+        } catch (\PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
     }
-    
+
+    public function execute($sql) {
+        $sth = $this->dbh->prepare($sql);
+        $result = $sth->execute();
+        return $result;
+    }
+
+    public function query($sql, $class) {
+        $sth = $this->dbh->prepare($sql);
+        $result = $sth->execute();
+        if (false !== $result) {
+            return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
+        }
+        return[];
+    }
+
 }
--- test.php
+++ test.php
@@ -1,13 +1,7 @@
 <?php
 
-include_once('sql.php');
-
-$Connect = mysqli_connect(HOST, USER, PASSWORD, DB);
-
-if(!$Connect) exit('Ошибка подключения');
-
-$Row = mysqli_fetch_assoc( mysqli_query($Connect, 'SELECT * FROM `users` WHERE `id` = 1'));
+require __DIR__ . '/autoload.php';
 
 
-print"$Row[password]";
-?>
+$users = \Models\User::findAll();
+var_dump($users);
